d76f7c538a17df0ddb3c47b3def6f0da3d1bb651
damnee562 <damnee562@gmail.com>
2017-07-27 13:42:29 +0800

    Added link to social media.

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -4,9 +4,17 @@ import { Container, Button } from 'semantic-ui-react'
 const Footer = () => {
     return (
         <Container id='footer' fluid>
-            <Button circular color='facebook' icon='facebook f' />
-            <Button circular color='linkedin' icon='linkedin' />
-            <Button circular color='black' icon='github' />
+            <a href='https://www.facebook.com/profile.php?id=100003107661326' target='_blank'>
+                <Button circular color='facebook' icon='facebook f' />
+            </a>
+
+            <a href='https://www.linkedin.com/in/%E8%B2%B4%E5%BB%B7-%E6%9B%BE-56920a145/' target='_blank'>
+                <Button circular color='linkedin' icon='linkedin' />
+            </a>
+
+            <a href='https://github.com/damnee562' target='_blank'>
+                <Button circular color='black' icon='github' />
+            </a>
         </Container>
     )
 }
