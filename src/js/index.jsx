1fd06abccd189bf4872ad6812016f5ca1cb5490c
damnee562 <damnee562@gmail.com>
2017-07-28 16:27:42 +0800

    Setup and configure redux.

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
 import App from './containers/App'
+import rootReducer from './reducers/RootReducer'
 import '../scss/style.scss'
 
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+)
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('app')
 )
---
+++ src/js/reducers/RootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+
+import projects from './Projects'
+
+const RootReducer = combineReducers({
+    projects
+})
+
+
+export default RootReducer
